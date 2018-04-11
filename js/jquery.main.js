f54e36303aad83cbcf186ea548a401d2093eb66e
obedgm <obedgm33@gmail.com>
2018-04-11 11:10:58 -0500

    Added videos to section

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -767,3 +767,56 @@ $(document).ready(function(){
 		$this.val() == '' && $this.val($this.attr('data-placeholder'));
 	});
 });
+
+
+
+ var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      // 3. This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '390',    
+          width: '50%',        
+          videoId: 'NI-NbDYfg4c',
+          events: {
+            'onStateChange': onPlayerStateChange
+          }
+        });
+        player1 = new YT.Player('player1', {
+            height: '390',
+            width: '49%',
+            videoId:'n7kLuXie64w',
+            events: {
+                'onStateChange': onPlayerStateChange
+            }
+        });
+      }
+        
+
+
+      // 4. The API will call this function when the video player is ready.
+      function onPlayerReady(event) {
+        event.target.playVideo();
+      }
+
+      // 5. The API calls this function when the player's state changes.
+      //    The function indicates that when playing a video (state=1),
+      //    the player should play for six seconds and then stop.
+      var done = false;
+      function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+          done = true;
+        }
+      }
+      function stopVideo() {
+        player.stopVideo();
+      }
+
+
+
